fe22afc5179ecab2d7ac31855bfd45ab66be04ef
Ruslan Primak <ruslan.primak@gmail.com>
2017-03-29 23:13:56 +0300

    Before changing RadioButtonStyle

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -1,13 +1,51 @@
+/*
+ * Copyright (c) 2017. Ruslan Primak
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Last modified 3/29/17 10:05 PM
+ */
+
 package com.example.android.quizapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.database.DatabaseUtilsCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.quizapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    public class CountGenerator {
+        private int counter = 0;
+
+        public String init() {
+            counter = 0;
+            return "" + counter;
+        }
+
+        public String next() {
+            counter += 1;
+            return "" + counter;
+        }
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setCounter(new CountGenerator());
+        //setContentView(R.layout.activity_main);
     }
 }
